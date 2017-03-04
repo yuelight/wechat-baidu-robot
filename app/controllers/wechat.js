78fda0ed9566b10657318015197d670bb3ac9092
yuelight <vaeqfe@gmail.com>
2017-03-04 16:18:42 +0800

    fix intend

--- app/controllers/wechat.js
+++ app/controllers/wechat.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const wechat = require('wechat');
+const User = mongoose.model('User');
 
 const jssdk = require('../libs/jssdk');
 
@@ -9,14 +10,16 @@ module.exports = function (app) {
     app.use('/wechat', router);
 };
 
-router.get('/hello', function(req, res, next) {
+router.get('/hello', function (req, res, next) {
     jssdk.getSignPackage(`http://120.27.99.227${req.url}`, function (err, signPackage) {
         if (err) {
             return next(err);
         }
 
         res.render('index', {
-            title: 'Hello Wechat from Aliyun ECS --> Express'
+            title: 'Hello Wechat from Aliyun ECS --> Express',
+            signPackage: signPackage,
+            pretty: true
         });
     });
 });
@@ -24,7 +27,7 @@ router.get('/hello', function(req, res, next) {
 const config = {
     token: 'dmIKhkJ65yZ4cdLGWU40',
     appid: 'wx24ed2d4b06ace0fe'
-}
+};
 
 const handleWechatRequest = wechat(config, function (req, res, next) {
     const message = req.weixin;
--- app/models/user.js
+++ app/models/user.js
@@ -1,19 +1,19 @@
 var mongoose = require('mongoose'),
-  Schema = mongoose.Schema;
+    Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    openid: {
-        type: String,
-        required: '`openid`是必填参数'
-    },
-    createdAt: {
-        type: Date,
-        required: '`createdAt`是必填参数'
-    },
-    conversationCount: {
-        type: Number,
-        default: 0
-    }
+        openid: {
+                type: String,
+                required: '`openid`是必填参数'
+        },
+        createdAt: {
+                type: Date,
+                required: '`createdAt`是必填参数'
+        },
+        conversationCount: {
+                type: Number,
+                default: 0
+        }
 });
 
 mongoose.model('User', UserSchema);
